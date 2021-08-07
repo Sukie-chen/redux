6cf4a2df2883e1145d2260d08a85ac960e3ea967
Sukie-chen <csq1410@163.com>
2021-08-07 16:23:04 +0800

    抽取单独形成redux

--- src/App.js
+++ src/App.js
@@ -1,39 +1,6 @@
-import React, {useContext, useEffect, useState} from "react";
+import React from "react";
 import './App.css';
-const appContext = React.createContext(null)
-const store = {
-  state: {
-    user: {name: 'Sunny', age: 18 }
-  },
-  setState(newState){
-    store.state = newState
-    store.listeners.map(fn => fn(store.state))
-  },
-  listeners: [],
-  subscribe(fn) {
-    store.listeners.push(fn)
-    return () => {
-      const index = store.listeners.indexOf(fn)
-      store.listeners.splice(index, 1)
-    }
-  }
-}
-// connect 将组件与全局状态联系在一起
-const connect = (Component) => {
-  return (props) => {
-    const {state, setState} = useContext(appContext)
-    const [, update] = useState({})
-    useEffect(()=> {
-      store.subscribe( ()=> {
-        update({})
-      })
-    }, [])
-    const dispatch = (action) => {
-      setState(reducer(state, action))
-    }
-    return <Component {...props} dispatch={dispatch} state={state}/>
-  }
-}
+import {appContext, store, connect} from "./redux";
 function App() {
   return (
     <appContext.Provider  className="App" value={store}>
@@ -44,18 +11,7 @@ function App() {
   );
 }
 
-const reducer = (state, {type, payload}) => {
-  if (type=== 'UPDATE') {
-    return {
-      ...state,
-      user: {
-        ...state.user,
-        ...payload
-      }
-    }
-  }else {}
-  return state
-}
+
 
 
 const Item1 = () => {
---
+++ src/redux.js
@@ -0,0 +1,48 @@
+import React, {useContext, useEffect, useState} from "react";
+
+export const appContext = React.createContext(null)
+export const store = {
+    state: {
+        user: {name: 'Sunny', age: 18 }
+    },
+    setState(newState){
+        store.state = newState
+        store.listeners.map(fn => fn(store.state))
+    },
+    listeners: [],
+    subscribe(fn) {
+        store.listeners.push(fn)
+        return () => {
+            const index = store.listeners.indexOf(fn)
+            store.listeners.splice(index, 1)
+        }
+    }
+}
+const reducer = (state, {type, payload}) => {
+    if (type=== 'UPDATE') {
+        return {
+            ...state,
+            user: {
+                ...state.user,
+                ...payload
+            }
+        }
+    }else {}
+    return state
+}
+// connect 将组件与全局状态联系在一起
+export const connect = (Component) => {
+    return (props) => {
+        const {state, setState} = useContext(appContext)
+        const [, update] = useState({})
+        useEffect(()=> {
+            store.subscribe( ()=> {
+                update({})
+            })
+        }, [])
+        const dispatch = (action) => {
+            setState(reducer(state, action))
+        }
+        return <Component {...props} dispatch={dispatch} state={state}/>
+    }
+}
